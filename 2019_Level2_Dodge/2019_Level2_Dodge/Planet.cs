075800f96df327a3fc1571c4edd2f2455897a68e
KiwisGithub <41028518+KiwisGithub@users.noreply.github.com>
2019-06-13 08:54:24 +1200

    Added everything

---
+++ 2019_Level2_Dodge/2019_Level2_Dodge/Planet.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Threading.Tasks;
+
+namespace _2019_Level2_Dodge
+{
+    class Planet
+    {
+        // declare fields to use in the class
+        public int x, y, width, height;//variables for the rectangle
+        public Image planetImage;//variable for the planet's image
+
+        public Rectangle planetRec;//variable for a rectangle to place our image in
+        public int score;
+        //Create a constructor (initialises the values of the fields)
+        public Planet(int spacing)
+        {
+            x = spacing;
+            y = 10;
+            width = 20;
+            height = 20;
+            planetImage = Image.FromFile("planet1.png");
+            planetRec = new Rectangle(x, y, width, height);
+
+
+        }
+
+        // Methods for the Planet class
+        public void drawPlanet(Graphics g)
+        {
+            planetRec = new Rectangle(x, y, width, height);
+            g.DrawImage(planetImage, planetRec);
+        }
+
+        public void movePlanet()
+        {
+            y += 5;
+
+            planetRec.Location = new Point(x, y);
+
+            if (planetRec.Location.Y > 400)
+            {
+                y = 20;
+                planetRec.Location = new Point(x, y);
+            }
+
+        }
+
+
+
+    }
+}
---
+++ 2019_Level2_Dodge/2019_Level2_Dodge/frmDodge.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2019_Level2_Dodge
+{
+    public partial class frmDodge : Form
+    {
+        Graphics g; //declare a graphics object called g
+                    // declare space for an array of 7 objects called planet 
+        Planet[] planet = new Planet[7];
+        Random yspeed = new Random();
+
+        Spaceship spaceship = new Spaceship();
+
+        bool left, right;
+        int score, lives;
+        string move;
+        public frmDodge()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 7; i++)
+            {
+                int x = 10 + (i * 75);
+                planet[i] = new Planet(x);
+            }
+
+
+        }
+
+        private void frmDodge_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void pnlGame_Paint(object sender, PaintEventArgs e)
+        {
+                //get the graphics used to paint on the panel control
+                g = e.Graphics;
+            //call the Planet class's DrawPlanet method to draw the image planet1 
+            for (int i = 0; i < 7; i++)
+            {
+
+                // generate a random number from 5 to 20 and put it in rndmspeed
+                int rndmspeed = yspeed.Next(5, 20);
+                planet[i].y += rndmspeed;
+
+                //call the Planet class's drawPlanet method to draw the images
+                planet[i].drawPlanet(g);
+            }
+            spaceship.drawSpaceship(g);
+
+
+        }
+
+        private void tmrPlanet_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                planet[i].movePlanet();
+            }
+            pnlGame.Invalidate();//makes the paint event fire to redraw the panel
+        }
+
+        private void frmDodge_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Left) { left = true; }
+            if (e.KeyData == Keys.Right) { right = true; }
+
+        }
+
+        private void tmrShip_Tick(object sender, EventArgs e)
+        {
+            if (right) // if right arrow key pressed
+            {
+                move = "right";
+                spaceship.moveSpaceship(move);
+            }
+            if (left) // if left arrow key pressed
+            {
+                move = "left";
+                spaceship.moveSpaceship(move);
+            }
+
+        }
+
+        private void frmDodge_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Left) { left = false; }
+            if (e.KeyData == Keys.Right) { right = false; }
+
+        }
+    }
+}
