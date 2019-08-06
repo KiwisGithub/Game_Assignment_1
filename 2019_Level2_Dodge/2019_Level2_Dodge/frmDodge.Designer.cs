c23d10cd4a3949b4bf0648a81bebf2510d39267e
School Local <tyakkaya@mmc.school.nz>
2019-08-06 14:07:40 +1200

    Changed games border to match main menus

--- 2019_Level2_Dodge/2019_Level2_Dodge/frmDodge.Designer.cs
+++ 2019_Level2_Dodge/2019_Level2_Dodge/frmDodge.Designer.cs
@@ -217,7 +217,7 @@
             this.Controls.Add(this.pnlGame);
             this.Controls.Add(this.mnuStart);
             this.DoubleBuffered = true;
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.KeyPreview = true;
             this.Name = "frmDodge";
