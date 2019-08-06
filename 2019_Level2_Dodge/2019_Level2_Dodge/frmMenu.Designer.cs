2eb6d79e7f3d7495e1d4de576cda48601d13e506
School Local <tyakkaya@mmc.school.nz>
2019-08-06 14:04:46 +1200

    Changed min max on HP

--- 2019_Level2_Dodge/2019_Level2_Dodge/frmMenu.Designer.cs
+++ 2019_Level2_Dodge/2019_Level2_Dodge/frmMenu.Designer.cs
@@ -125,9 +125,24 @@
             // numericUpDown1
             // 
             this.numericUpDown1.Location = new System.Drawing.Point(670, 504);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
             this.numericUpDown1.Name = "numericUpDown1";
             this.numericUpDown1.Size = new System.Drawing.Size(147, 20);
             this.numericUpDown1.TabIndex = 9;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
             // 
             // frmMenu
             // 
