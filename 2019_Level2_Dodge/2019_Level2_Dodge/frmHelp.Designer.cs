39fa4720e1c6e8518cfd5c88b8d4f4902267416c
School Local <tyakkaya@mmc.school.nz>
2019-08-13 14:10:53 +1200

    Added a help meny

---
+++ 2019_Level2_Dodge/2019_Level2_Dodge/frmHelp.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace _2019_Level2_Dodge
+{
+    partial class frmHelp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmHelp));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tmrPlanet = new System.Windows.Forms.Timer(this.components);
+            this.tmrShip = new System.Windows.Forms.Timer(this.components);
+            this.tmrMouse = new System.Windows.Forms.Timer(this.components);
+            this.tmrMissile = new System.Windows.Forms.Timer(this.components);
+            this.tmrMissile2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("OCR A Std", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 34);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Move:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("OCR A Std", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.Location = new System.Drawing.Point(359, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 34);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Aim:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("OCR A Std", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label3.Location = new System.Drawing.Point(839, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(135, 34);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Fire:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("OCR A Std", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label4.Location = new System.Drawing.Point(153, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 34);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "WASD";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("OCR A Std", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label5.Location = new System.Drawing.Point(476, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(245, 34);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Move Mouse";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("OCR A Std", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label6.Location = new System.Drawing.Point(980, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(153, 34);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Mouse1";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("OCR A Std", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label7.Location = new System.Drawing.Point(1399, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(84, 34);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Esc";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("OCR A Std", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label8.Location = new System.Drawing.Point(1258, 9);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(135, 34);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Exit:";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Location = new System.Drawing.Point(0, 57);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1500, 787);
+            this.panel1.TabIndex = 9;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseEnter += new System.EventHandler(this.panel1_MouseEnter);
+            this.panel1.MouseLeave += new System.EventHandler(this.panel1_MouseLeave);
+            this.panel1.MouseHover += new System.EventHandler(this.panel1_MouseHover);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            // 
+            // tmrPlanet
+            // 
+            this.tmrPlanet.Enabled = true;
+            this.tmrPlanet.Interval = 60;
+            this.tmrPlanet.Tick += new System.EventHandler(this.tmrPlanet_Tick);
+            // 
+            // tmrShip
+            // 
+            this.tmrShip.Enabled = true;
+            this.tmrShip.Interval = 5;
+            this.tmrShip.Tick += new System.EventHandler(this.tmrShip_Tick);
+            // 
+            // tmrMouse
+            // 
+            this.tmrMouse.Enabled = true;
+            this.tmrMouse.Interval = 2;
+            this.tmrMouse.Tick += new System.EventHandler(this.tmrMouse_Tick);
+            // 
+            // tmrMissile
+            // 
+            this.tmrMissile.Enabled = true;
+            this.tmrMissile.Interval = 1000;
+            this.tmrMissile.Tick += new System.EventHandler(this.tmrMissile_Tick);
+            // 
+            // tmrMissile2
+            // 
+            this.tmrMissile2.Enabled = true;
+            this.tmrMissile2.Interval = 10000;
+            this.tmrMissile2.Tick += new System.EventHandler(this.tmrMissile2_Tick);
+            // 
+            // frmHelp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::_2019_Level2_Dodge.Properties.Resources.bruh2;
+            this.ClientSize = new System.Drawing.Size(1495, 843);
+            this.ControlBox = false;
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmHelp";
+            this.Text = "Planet Doge | Help";
+            this.Load += new System.EventHandler(this.frmHelp_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frmHelp_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.frmHelp_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer tmrPlanet;
+        private System.Windows.Forms.Timer tmrShip;
+        private System.Windows.Forms.Timer tmrMouse;
+        private System.Windows.Forms.Timer tmrMissile;
+        private System.Windows.Forms.Timer tmrMissile2;
+    }
+}
--- 2019_Level2_Dodge/2019_Level2_Dodge/frmMenu.cs
+++ 2019_Level2_Dodge/2019_Level2_Dodge/frmMenu.cs
@@ -32,10 +32,16 @@ namespace _2019_Level2_Dodge
 
         private void pictureBox5_Click(object sender, EventArgs e)
         {
-            DialogResult result1 = MessageBox.Show("You are beyond help.",
-            "Help",
-            MessageBoxButtons.OK,
-            MessageBoxIcon.Question);
+
+            frmHelp helpForm = new frmHelp();
+            //Application.Exit();
+            this.Close();
+            helpForm.Show();
+
+           // DialogResult result1 = MessageBox.Show("You are beyond help.",
+           // "Help",
+           // MessageBoxButtons.OK,
+          //  MessageBoxIcon.Question);
             //MessageBox.Show("Use the left and right arrow keys to move the spaceship. \n Don't get hit by the planets! \n Every planet that gets past scores a point. \n If a planet hits a spaceship a life is lost! \n \n Enter your Name press tab and enter the number of lives \n Click Start to begin", "Game Instructions");
         }
 
